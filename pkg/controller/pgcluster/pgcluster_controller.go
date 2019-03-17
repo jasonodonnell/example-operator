8045c44a588a7815c86777575a31bb6711b36b4f
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2019-03-17 15:45:56 -0400

    Update

--- pkg/controller/pgcluster/pgcluster_controller.go
+++ pkg/controller/pgcluster/pgcluster_controller.go
@@ -2,7 +2,7 @@ package pgcluster
 
 import (
 	"context"
-	"fmt"
+	"reflect"
 
 	pgclusterv1alpha1 "github.com/jasonodonnell/example-operator/pkg/apis/pgcluster/v1alpha1"
 
@@ -10,6 +10,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -89,8 +90,8 @@ func (r *ReconcilePGCluster) Reconcile(request reconcile.Request) (reconcile.Res
 	reqLogger.Info("Reconciling PGCluster")
 
 	// Fetch the PGCluster instance
-	instance := &pgclusterv1alpha1.PGCluster{}
-	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
+	pgcluster := &pgclusterv1alpha1.PGCluster{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, pgcluster)
 	if err != nil {
 		if errors.IsNotFound(err) {
 			// Request object not found, could have been deleted after reconcile request.
@@ -102,55 +103,84 @@ func (r *ReconcilePGCluster) Reconcile(request reconcile.Request) (reconcile.Res
 		return reconcile.Result{}, err
 	}
 
-	// Define a new deployment object
-	deployment := newDeploymentForCR(instance)
-	fmt.Println(deployment.String())
-
-	// Set PGCluster instance as the owner and controller
-	if err := controllerutil.SetControllerReference(instance, deployment, r.scheme); err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// Check if this Pod already exists
+	// Check if the deployment already exists, if not create a new one
 	found := &appsv1.Deployment{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: deployment.Name, Namespace: deployment.Namespace}, found)
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pgcluster.Name, Namespace: pgcluster.Namespace}, found)
 	if err != nil && errors.IsNotFound(err) {
-		reqLogger.Info("Creating a new deployment", "deployment.Namespace", deployment.Namespace, "deployment.Name", deployment.Name)
-		err = r.client.Create(context.TODO(), deployment)
+		// Define a new deployment
+		dep := r.newDeploymentForPGCluster(pgcluster)
+		reqLogger.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		err = r.client.Create(context.TODO(), dep)
 		if err != nil {
+			reqLogger.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
 			return reconcile.Result{}, err
 		}
-
-		// Deployment created successfully - don't requeue
-		return reconcile.Result{}, nil
+		// Deployment created successfully - return and requeue
+		return reconcile.Result{Requeue: true}, nil
 	} else if err != nil {
+		reqLogger.Error(err, "Failed to get Deployment")
 		return reconcile.Result{}, err
 	}
 
-	// Pod already exists - don't requeue
-	reqLogger.Info("Skip reconcile: Pod already exists", "deployment.Namespace", found.Namespace, "deployment.Name", found.Name)
+	// Ensure the deployment size is the same as the spec
+	size := pgcluster.Spec.Size
+	if *found.Spec.Replicas != size {
+		found.Spec.Replicas = &size
+		err = r.client.Update(context.TODO(), found)
+		if err != nil {
+			reqLogger.Error(err, "Failed to update Deployment", "Deployment.Namespace", found.Namespace, "Deployment.Name", found.Name)
+			return reconcile.Result{}, err
+		}
+		// Spec updated - return and requeue
+		return reconcile.Result{Requeue: true}, nil
+	}
+
+	// Update the pgcluster status with the pod names
+	// List the pods for this pgcluster's deployment
+	podList := &corev1.PodList{}
+	labelSelector := labels.SelectorFromSet(labelsForPGCluster(pgcluster.Name))
+	listOps := &client.ListOptions{Namespace: pgcluster.Namespace, LabelSelector: labelSelector}
+	err = r.client.List(context.TODO(), listOps, podList)
+	if err != nil {
+		reqLogger.Error(err, "Failed to list pods", "pgcluster.Namespace", pgcluster.Namespace, "pgcluster.Name", pgcluster.Name)
+		return reconcile.Result{}, err
+	}
+	podNames := getPodNames(podList.Items)
+
+	// Update status.Nodes if needed
+	if !reflect.DeepEqual(podNames, pgcluster.Status.Nodes) {
+		pgcluster.Status.Nodes = podNames
+		err := r.client.Status().Update(context.TODO(), pgcluster)
+		if err != nil {
+			reqLogger.Error(err, "Failed to update pgcluster status")
+			return reconcile.Result{}, err
+		}
+	}
+
 	return reconcile.Result{}, nil
 }
 
-func newDeploymentForCR(cr *pgclusterv1alpha1.PGCluster) *appsv1.Deployment {
-	fmt.Println(cr.Name)
-	labels := map[string]string{
-		"app": cr.Name,
-	}
-	return &appsv1.Deployment{
+func (r *ReconcilePGCluster) newDeploymentForPGCluster(cr *pgclusterv1alpha1.PGCluster) *appsv1.Deployment {
+	replicas := cr.Spec.Size
+	ls := labelsForPGCluster(cr.Name)
+
+	dep := &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name: cr.Name,
+			Name:      cr.Name,
+			Namespace: cr.Namespace,
 		},
 		Spec: appsv1.DeploymentSpec{
-			Replicas: &cr.Spec.Size,
+			Replicas: &replicas,
 			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": "demo",
-				},
+				MatchLabels: ls,
 			},
 			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
-					Labels: labels,
+					Labels: ls,
 				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
@@ -170,4 +200,19 @@ func newDeploymentForCR(cr *pgclusterv1alpha1.PGCluster) *appsv1.Deployment {
 			},
 		},
 	}
+	controllerutil.SetControllerReference(cr, dep, r.scheme)
+	return dep
+}
+
+func labelsForPGCluster(name string) map[string]string {
+	return map[string]string{"app": "postgres", "pgcluster_cr": name}
+}
+
+// getPodNames returns the pod names of the array of pods passed in
+func getPodNames(pods []corev1.Pod) []string {
+	var podNames []string
+	for _, pod := range pods {
+		podNames = append(podNames, pod.Name)
+	}
+	return podNames
 }
